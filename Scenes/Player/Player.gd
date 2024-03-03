6e22ed55a03ae90845adcb84d4374c33c968c443
Rod Laycock <rod.laycock@gmail.com>
2024-03-03 14:49:31 +0000

    Added assets

---
+++ Scenes/Player/Player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+#
+#
+
+# Player Health
+#	100% = Full Health
+#	
+#	< 20% = Very Damaged
+
+const SPEED = 300.0
+#const JUMP_VELOCITY = -400.0
+#
+## Get the gravity from the project settings to be synced with RigidBody nodes.
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#
+#
+func process(delta):
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
