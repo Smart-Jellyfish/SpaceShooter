0794ffa240e27a911c4fc89a640e01bc452fafc0
Rod Laycock <rod.laycock@gmail.com>
2024-03-05 20:39:46 +0000

    added random direction so the meteors go at angles

--- Scenes/Meteor/meteor.gd
+++ Scenes/Meteor/meteor.gd
@@ -1,20 +1,22 @@
 extends Area2D
 
-var ROTATION_SPEED: float
-var ROTATE_CLOCKWISE: bool
-var VELOCITY: float
-var DIRECTION_X: float
+var rotation_speed: float
+var rotate_clockwise: bool
+var velocity: float
+var direction_x: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Set random rotation speed
-	ROTATION_SPEED = randf_range(0.0000000000001, 5.0)
+	rotation_speed = randf_range(0.0000000000001, 5.0)
 	
 	# Set ramdom direction
-	ROTATE_CLOCKWISE = randi_range(0, 100) % 2 == 1 
+	rotate_clockwise = randi_range(0, 100) % 2 == 1 
 	
 	# Set a random velocity
-	VELOCITY = randf_range(200, 500)
+	velocity = randf_range(200, 500)
+	
+	direction_x = randf_range(-1, 1)
 	
 	# Set random start position
 	var width  = get_viewport().get_visible_rect().size[0]
@@ -23,10 +25,10 @@ func _ready():
 	position = Vector2(random_x, random_y)
 
 func _process(delta):
-		position += Vector2(0, 1.0) * VELOCITY * delta
+		position += Vector2(direction_x, 1.0) * velocity * delta
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	if ROTATE_CLOCKWISE:
-		rotation += 1 * ROTATION_SPEED * delta
+	if rotate_clockwise:
+		rotation += 1 * rotation_speed * delta
 	else:
-		rotation += -1 * ROTATION_SPEED * delta
+		rotation += -1 * rotation_speed * delta
