74a8c53449255aa7a792d914e3efac2006647fc0
Rod Laycock <rod.laycock@gmail.com>
2024-03-09 11:13:24 +0000

    added press space to play again

---
+++ Scenes/GameOver/game_over.gd
@@ -0,0 +1,8 @@
+extends Control
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("Fire Primary"):
+		get_tree().change_scene_to_file("res://Scenes/Level/level.tscn")
+
+
